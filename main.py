c4cb9ad0f273134c2793c0bda1a82ab1663a94f1
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2025-12-05 19:37:44 +0400

    Add poll creation and group command handling

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import os
 import logging
+from enum import Enum
+import sqlite3
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
+
+
+class UserConversationState(Enum):
+    NONE = 0
+    SETTING_TITLE = 1
+
+
+db = sqlite3.connect('bot.db')
+cur = db.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS polls(id INTEGER PRIMARY KEY, owner INTEGER, title TEXT);")
+db.commit()
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,14 +27,50 @@ if not token:
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="""Команды:
-/new - создать голосование
-""")
+    if update.effective_chat.type == filters.ChatType.PRIVATE:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="""Команды:
+/new - создать голосование""")
+        return
+    if len(context.args) == 1:
+        try:
+            poll_id = int(context.args[0])
+        except ValueError:
+            await update.message.reply_text("Не правильно указан номер опроса.")
+            return
+        cursor = cur.execute("SELECT owner,title FROM polls WHERE id = ?", [poll_id])
+        poll = cursor.fetchone()
+        print(poll, update.effective_chat.id)
+        if poll is None:
+            await update.message.reply_text("Не найден опрос.")
+            return
+        if poll[0] != update.effective_user.id:
+            return
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"тут будет опрос #{poll_id}")
+
+
+async def new(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["state"] = UserConversationState.SETTING_TITLE
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Напишите заголовок опроса.")
+
+
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.user_data["state"] != UserConversationState.SETTING_TITLE:
+        return
+    cursor = cur.execute("INSERT INTO polls(owner,title) VALUES(?,?) RETURNING id;",
+                         [update.effective_chat.id, update.message.text])
+    new_id = cursor.fetchone()[0]
+    db.commit()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Создан опрос #{new_id}")
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(token).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+    new_handler = CommandHandler('new', new, filters.ChatType.PRIVATE)
+    application.add_handler(new_handler)
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND) & filters.ChatType.PRIVATE, message)
+    application.add_handler(message_handler)
 
     application.run_polling()
